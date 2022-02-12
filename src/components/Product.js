10f267c923bb50a4ddd7c614265654e367920920
thoriqnoer <thoriq.noer@gmail.com>
2022-02-12 14:23:43 +0700

    product details

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import { Card } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const Product = ({ product }) => {
   return (
     <Card className="my-3 p-3 rounded">
-      <Card.Img src={product.image} variant="top" />
+      <Link to={`/product/${product._id}`}>
+        <Card.Img src={product.image} variant="top" />
+      </Link>
+
+      <Card.Body>
+        <Link to={`/product/${product._id}`}>
+          <Card.Title as="div">
+            <strong>{product.name}</strong>
+          </Card.Title>
+        </Link>
+
+        <Card.Text as="h3">$ {product.price}</Card.Text>
+      </Card.Body>
     </Card>
   );
 };
