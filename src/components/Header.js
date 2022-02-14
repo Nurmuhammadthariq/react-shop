25aa993d15864dfe888242a13999a3520b8fffb6
thoriqnoer <thoriq.noer@gmail.com>
2022-02-14 14:29:02 +0700

    Link to cart

--- src/components/Header.js
+++ src/components/Header.js
@@ -13,9 +13,11 @@ const Header = () => {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ml-auto">
-              <Nav.Link href="/cart">
-                <i className="fas fa-shopping-cart" /> Cart
-              </Nav.Link>
+              <LinkContainer to="/cart">
+                <Nav.Link>
+                  <i className="fas fa-shopping-cart" /> Cart
+                </Nav.Link>
+              </LinkContainer>
               <Nav.Link href="/login">
                 <i className="fas fa-user" /> Sign In
               </Nav.Link>
