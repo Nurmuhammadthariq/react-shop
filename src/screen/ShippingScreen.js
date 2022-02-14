1b888398274f6444cdc90dc1c1fed442cffc02b8
thoriqnoer <thoriq.noer@gmail.com>
2022-02-14 19:51:57 +0700

    remove form group

--- src/screen/ShippingScreen.js
+++ src/screen/ShippingScreen.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Form, Button, FormGroup } from 'react-bootstrap';
+import { Form, Button } from 'react-bootstrap';
 import { useSelector, useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 
